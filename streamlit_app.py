a86bda2569e7e8dbf3f55e98c818d26f0761e522
ilfan-tech <ilfansans@gmail.com>
2025-12-11 16:29:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
-st.title("This is a title")
-st.title("_NTP_ is :blue[cool] :sunglasses:")
-import streamlit as st
+col1, col2, col3 = st.columns(3)
 
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
 
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
