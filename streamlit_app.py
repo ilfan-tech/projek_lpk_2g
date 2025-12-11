2d991be0f641a5a6169a4e78c86e1eda8cc0f29f
ilfan-tech <ilfansans@gmail.com>
2025-12-11 16:28:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("This is a title")
-st.title("_NTP_ is :blue[cool] :sunglasses:")import streamlit as st
+st.title("_NTP_ is :blue[cool] :sunglasses:")
+import streamlit as st
 
 
