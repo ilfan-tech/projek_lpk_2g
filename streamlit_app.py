7b1fb125407cef855052942e4c1e52d12c1a7983
ilfan-tech <ilfansans@gmail.com>
2025-12-11 16:25:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+st.title("This is a title")
+st.title("_NTP_ is :blue[cool] :sunglasses:")import streamlit as st
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
